f8c0a17f2820a0209b357c7bc6a1866b492000fc
AdlinM <adlin.hiker@gmail.com>
2018-01-09 10:46:12 +0700

    resolve highchart data

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -7,7 +7,7 @@
       <div id="dasboard" class="row">
         <div class="col-sm-5">
           <h3 class="text-center">Pie Chart</h3>
-          <pie v-if="ticketstats.week !== null" :labels="ticketstats.week.labels" :datas="ticketstats.week.datas" :backgroundColors="ticketstats.week.colors"/>
+          <pie v-if="ticketstats.month !== null" :labels="ticketstats.month.labels" :datas="ticketstats.month.datas" :backgroundColors="ticketstats.month.colors"/>
         </div>
         <div class="col-sm-offset-2 col-sm-5">
           <h3 class="text-center">High Chart</h3>
@@ -76,7 +76,7 @@ export default {
       msg: 'this is homepage',
       setInterval: null,
       ticketstats: {
-        week: null
+        month: null
       },
       options: this.initial,
       pieOptions: null
@@ -86,9 +86,8 @@ export default {
     getChartData (period) {
       axios.get(window.AppConfig.apiUrl + 'ticketstats.' + period)
       .then(response => {
-        console.log('Get In Chart')
-        console.log(response)
         if (Object.keys(response.data).length) {
+          console.log('set data chart')
           let stats = {labels: Object.keys(response.data), datas: Object.values(response.data), colors: []}
           for (let i = 0; i < Object.keys(response.data).length; i++) {
             stats.colors.push(window.statusText[Object.keys(response.data)[i]].color)
@@ -104,35 +103,34 @@ export default {
     getHighChartData (period) {
       axios.get(window.AppConfig.apiUrl + 'ticketstats.' + period)
       .then(response => {
-        console.log('Get In Highchart')
-        console.log(response)
-        if (Object.keys(response.data).length) {
-          console.log('set data Highchart')
-          this.pieOptions = {
-            chart: {
-              type: 'pie',
-              options3d: {
-                enabled: true,
-                alpha: 45
-              }
-            },
-            title: {
-              text: 'Title test'
-            },
-            subtitle: {
-              text: 'subtitle text'
-            },
-            plotOptions: {
-              pie: {
-                innerSize: 100,
-                depth: 45
-              }
-            },
-            series: [{
-              name: 'test amount',
-              data: response.data
-            }]
-          }
+        let dataChart = []
+        Object.keys(response.data).forEach(function (key) {
+          dataChart.push([key, parseInt(response.data[key])])
+        })
+        this.pieOptions = {
+          chart: {
+            type: 'pie',
+            options3d: {
+              enabled: true,
+              alpha: 45
+            }
+          },
+          title: {
+            text: 'Title test'
+          },
+          subtitle: {
+            text: 'subtitle text'
+          },
+          plotOptions: {
+            pie: {
+              innerSize: 100,
+              depth: 45
+            }
+          },
+          series: [{
+            name: 'test amount',
+            data: dataChart
+          }]
         }
       })
       .catch(error => {
